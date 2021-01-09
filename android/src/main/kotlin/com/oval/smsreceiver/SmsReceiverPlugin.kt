d6c987dcd9a535d116599036c968f9ef9884f9d1
Mahdi Arabpour <mahdi.a.200066@gmail.com>
2021-01-09 11:17:46 +0330

    set isListening to false when unregistered

--- android/src/main/kotlin/com/oval/smsreceiver/SmsReceiverPlugin.kt
+++ android/src/main/kotlin/com/oval/smsreceiver/SmsReceiverPlugin.kt
@@ -11,78 +11,79 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class SmsReceiverPlugin(private val activity: Activity,
-                        private val methodChannel: MethodChannel) : MethodCallHandler {
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val channel = MethodChannel(registrar.messenger(), "com.oval.smsreceiver")
-            channel.setMethodCallHandler(SmsReceiverPlugin(registrar.activity(), channel))
-        }
-
-        const val TAG: String = "FLUTTER-SMS-RECEIVER"
+                        private val methodChannel: MethodChannel): MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "com.oval.smsreceiver")
+      channel.setMethodCallHandler(SmsReceiverPlugin(registrar.activity(), channel))
     }
 
-    private val smsBroadcastReceiver by lazy { SMSBroadcastReceiver() }
-    private var isListening: Boolean = false
+    const val TAG:String = "FLUTTER-SMS-RECEIVER"
+  }
+
+  private val smsBroadcastReceiver by lazy { SMSBroadcastReceiver() }
+  private var isListening: Boolean = false
 
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        when (call.method) {
-            "getPlatformVersion" -> {
-                result.success("Android ${android.os.Build.VERSION.RELEASE}")
-            }
-            "startListening" -> {
-                synchronized(this) {
-                    if (!isListening) {
-                        isListening = true
-                        startListening()
-                        Log.d(TAG, "SMS Receiver Registered")
-                        result.success(true)
-                    } else {
-                        Log.d(TAG, "Preventing register multiple SMS receiver")
-                        result.success(false)
-                    }
-                }
-            }
-            "stopListening" -> {
-                stopListening()
-                result.success()
-            }
-            else -> {
-                result.notImplemented()
-            }
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when(call.method) {
+      "getPlatformVersion" -> {
+        result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      }
+      "startListening" -> {
+        synchronized(this){
+          if(!isListening){
+            isListening = true
+            startListening()
+            Log.d(TAG, "SMS Receiver Registered")
+            result.success(true)
+          }
+          else{
+            Log.d(TAG, "Preventing register multiple SMS receiver")
+            result.success(false)
+          }
         }
+      }
+      "stopListening" -> {
+        stopListening()
+      }
+      else -> {
+        result.notImplemented()
+      }
     }
+  }
 
-    private fun stopListening() {
-        activity.unregisterReceiver(smsBroadcastReceiver)
-        isListening = false
+  private fun stopListening() {
+    if (isListening) {
+      activity.unregisterReceiver(smsBroadcastReceiver)
+      isListening = false
     }
+  }
 
-    private fun startListening() {
-        val client = SmsRetriever.getClient(activity)
-        val retriever = client.startSmsRetriever()
-        retriever.addOnSuccessListener {
-            val listener = object : SMSBroadcastReceiver.Listener {
-                override fun onSMSReceived(otp: String) {
-                    methodChannel.invokeMethod("onSmsReceived", otp)
-                    activity.unregisterReceiver(smsBroadcastReceiver)
-                    isListening = false
-                }
-
-                override fun onTimeout() {
-                    methodChannel.invokeMethod("onTimeout", null)
-                    activity.unregisterReceiver(smsBroadcastReceiver)
-                    isListening = false
-                }
-            }
-            smsBroadcastReceiver.injectListener(listener)
-            activity.registerReceiver(smsBroadcastReceiver,
-                    IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION))
+  private fun startListening() {
+    val client = SmsRetriever.getClient(activity)
+    val retriever = client.startSmsRetriever()
+    retriever.addOnSuccessListener {
+      val listener = object:SMSBroadcastReceiver.Listener {
+        override fun onSMSReceived(message: String) {
+          methodChannel.invokeMethod("onSmsReceived", message)
+          activity.unregisterReceiver(smsBroadcastReceiver)
+          isListening = false
         }
-        retriever.addOnFailureListener {
-            methodChannel.invokeMethod("onFailureListener", null)
-            isListening = false
+        override fun onTimeout() {
+          methodChannel.invokeMethod("onTimeout", null)
+          activity.unregisterReceiver(smsBroadcastReceiver)
+          isListening = false
         }
+      }
+      smsBroadcastReceiver.injectListener(listener)
+      activity.registerReceiver(smsBroadcastReceiver,
+              IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION))
     }
+    retriever.addOnFailureListener {
+      methodChannel.invokeMethod("onFailureListener", null)
+      isListening = false
+    }
+  }
 }
